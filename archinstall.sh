cc961488cf0dbc8236cf7390bc71d8516a36ed49
Paul Ritterbush <ritterbush@berkeley.edu>
2020-09-07 00:16:22 -0700

    Update archinstall.sh

--- archinstall.sh
+++ archinstall.sh
@@ -44,7 +44,7 @@ COMMENT
 #Update System Clock
 timedatectl set-ntp true
 
-#Check if script was previously run and partitions previously made with it. If so, wipe them
+#Wipes the disk, and in particular it wipes the partitions previously made first, if this script was already run 
 ls /dev/"$rootpart" > /dev/null 2>&1 && wipefs --all --force /dev/"$rootpart"
 sleep 1
 ls /dev/"$efipart" > /dev/null 2>&1 && wipefs --all --force /dev/"$efipart"
@@ -61,17 +61,17 @@ sleep 2
 
 sleep 2
 
-(echo p; echo q) | fdisk /dev/"$disk"
+#(echo p; echo q) | fdisk /dev/"$disk"
 
-sleep 2
+#sleep 2
 
 #Make file systems for and mount partitions
 mkfs.fat -F32 /dev/"$efipart"
 mkfs.ext4 /dev/"$rootpart"
-#mkdir -p /mnt/efi
-mkdir -p /mnt/boot
-mount /dev/"$efipart" /mnt/boot
-#mount /dev/"$efipart" /mnt/efi
+mkdir -p /mnt/efi
+#mkdir -p /mnt/boot
+#mount /dev/"$efipart" /mnt/boot
+mount /dev/"$efipart" /mnt/efi
 mount /dev/"$rootpart" /mnt
 
 pacman -Syy
@@ -80,25 +80,14 @@ echo Y | pacman -S archlinux-keyring
 #Send good USA sites to the top of the  mirrorlist
 sed -i '6i\Server = http://mirror.arizona.edu/archlinux/$repo/os/$arch\nServer = https://mirror.arizona.edu/archlinux/$repo/os/$arch\nServer = http://mirrors.ocf.berkeley.edu/archlinux/$repo/os/$arch\nServer = https://mirrors.ocf.berkeley.edu/archlinux/$repo/os/$arch\nServer = http://arch.mirror.constant.com/$repo/os/$arch\nServer = https://arch.mirror.constant.com/$repo/os/$arch\nServer = http://mirrors.kernel.org/archlinux/$repo/os/$arch\nServer = https://mirrors.kernel.org/archlinux/$repo/os/$arch\nServer = http://mirrors.rit.edu/archlinux/$repo/os/$arch\nServer = https://mirrors.rit.edu/archlinux/$repo/os/$arch\nServer = http://mirrors.rutgers.edu/archlinux/$repo/os/$arch\nServer = https://mirrors.rutgers.edu/archlinux/$repo/os/$arch\nServer = http://ca.us.mirror.archlinux-br.org/$repo/os/$arch' /etc/pacman.d/mirrorlist
 
-#Install just the basic features
-pacstrap /mnt base #Moved to pacman install: linux linux-firmware base-devel sysfsutils usbutils e2fsprogs dosfstools mtools inetutils netctl dhcpcd device-mapper cryptsetup less lvm2
+#Install just the bare minimum until chroot
+pacstrap /mnt base
 
+#Generate fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
-#Chroot from here. Normal way:
-#arch-chroot /mnt
-
-
-<<COMMENT
-
-        This here file will be created and then executed in the
-        chrooted environment.
-
-COMMENT
-
-#Send script that executes the below to the new Arch system
+#Create the chroot script that executes inside the new Arch system 
 cat > /mnt/chrootfile.sh <<End-of-message
-
 
 #Set Timezone
 ln -sf /usr/share/zoneinfo/"$timezone" /etc/localtime
@@ -126,23 +115,22 @@ useradd -m -G wheel,audio,video "$username"
 [ "$cpu" == intel ] && (echo; echo; echo Y) | pacman -S linux linux-firmware intel-ucode
 [ "$cpu" == other ] && (echo; echo; echo Y) | pacman -S linux linux-firmware
 
-#Old:base-devel linux linux-firmware lvm2
+#Old included:base-devel lvm2
 
 
+#Old steps related to LVM--not used here, and maybe old info
 #Enable mkinitcpio hooks for busybox-based initramfs
-
 #sed -i '52s&block&block lvm2&' /etc/mkinitcpio.conf
-
 #The systemd version was not found as a valid hook
 #See below for more on busybox vs. systemd hooks:
 #https://wiki.archlinux.org/index.php/Talk:Mkinitcpio#Improvements_for_the_Common_hooks_table_and_section_about_systemd_hook
 #https://wiki.archlinux.org/index.php/LVM#Configure_mkinitcpio
-
 #should not need to do this
 #mkinitcpio -P
 
-#End additional LVM steps
 
+
+#Will include this once we specify installing sudo above
 #Give the wheel group root priviledges
 #sed -i 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers #Not advised; don't make typos here, or use visudo
 
@@ -160,12 +148,12 @@ echo Y | pacman -S grub efibootmgr #OLD:os-prober amd-ucode dosfstools; os-probe
 
 #Other programs that were once in base or that may be worth getting: sysfsutils usbutils e2fsprogs dosfstools mtools inetutils netctl dhcpcd device-mapper cryptsetup less lvm2 openssh vim zsh man-db man-pages
 
-#mkdir /efi
-mkdir -p /boot/efi
-#mount /dev/$efipart /efi #https://wiki.archlinux.org/index.php/EFI_system_partition#Mount_the_partition
-mount /dev/$efipart /boot/efi #https://wiki.archlinux.org/index.php/EFI_system_partition#Mount_the_partition
-#grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB #https://wiki.archlinux.org/index.php/GRUB#UEFI_systems
-grub-install --target=x86_64-efi --efi-directory=boot/efi --bootloader-id=GRUB #https://wiki.archlinux.org/index.php/GRUB#UEFI_systems
+mkdir /efi
+#mkdir -p /boot/efi
+mount /dev/$efipart /efi #https://wiki.archlinux.org/index.php/EFI_system_partition#Mount_the_partition
+#mount /dev/$efipart /boot/efi
+grub-install --target=x86_64-efi --efi-directory=efi --bootloader-id=GRUB #https://wiki.archlinux.org/index.php/GRUB#UEFI_systems
+#grub-install --target=x86_64-efi --efi-directory=boot/efi --bootloader-id=GRUB
 
 grub-mkconfig -o /boot/grub/grub.cfg
 
@@ -174,8 +162,7 @@ grub-mkconfig -o /boot/grub/grub.cfg
 #Clean up
 rm /mnt/chrootfile.sh
 
-# Good idea to unmount the USB drive before exiting chroot; then,
-
+# Good idea to unmount the USB drive before exiting chroot
 umount -a
 exit
 
@@ -186,7 +173,5 @@ chmod +x /mnt/chrootfile.sh
 
 #Execute it
 arch-chroot /mnt ./chrootfile.sh
-
-
 
 echo done
