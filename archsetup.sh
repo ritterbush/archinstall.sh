a3252c809fd78a786f28251fae47853a9d0174a5
Paul Ritterbush <ritterbush@berkeley.edu>
2020-09-11 22:20:41 -0700

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -46,10 +46,35 @@ cp /etc/xdg/picom.conf.example ~/.config/picom/picom.conf
 # Run again with different numbers to change
 
 ~/.local/bin/alacritty-opacity.sh 70
+#~/.local/bin/dwm-opacity.sh 70
+sed -i "s/static const unsigned int baralpha = .*/static const unsigned int baralpha = 0xb3;/" ~/Programs/dwm/config.def.h
+#~/.local/bin/wallpaper-and-colors.sh ~/Pictures/Wallpapers/fall-autumn-red-season.jpg
+sed -i "5s|.*|filepath=/home/${username}/Pictures/Wallpapers/fall-autumn-red-season.jpg|" "$HOME"/.local/bin/wallpaper-and-colors.sh
+xwallpaper --zoom ~/Pictures/Wallpapers/fall-autumn-red-season.jpg
+sed -i "s/static const char norm_fg\[\] = .*/$(sed -n 1p "$HOME"/.cache/wal/colors-wal-dwm.h)/" "$HOME"/Programs/dwm/config.def.h
+sed -i "s/static const char norm_bg\[\] = .*/$(sed -n 2p "$HOME"/.cache/wal/colors-wal-dwm.h)/" "$HOME"/Programs/dwm/config.def.h
+sed -i "s/static const char norm_border\[\] = .*/$(sed -n 3p "$HOME"/.cache/wal/colors-wal-dwm.h)/" "$HOME"/Programs/dwm/config.def.h
+sed -i "s/static const char sel_fg\[\] = .*/$(sed -n 5p "$HOME"/.cache/wal/colors-wal-dwm.h)/" "$HOME"/Programs/dwm/config.def.h
+sed -i "s/static const char sel_bg\[\] = .*/$(sed -n 6p "$HOME"/.cache/wal/colors-wal-dwm.h)/" "$HOME"/Programs/dwm/config.def.h
+sed -i "s/static const char sel_border\[\] = .*/$(sed -n 7p "$HOME"/.cache/wal/colors-wal-dwm.h)/" "$HOME"/Programs/dwm/config.def.h
 
-(echo "$password"; echo) | sudo -S pacman -S os-prober
-~/.local/bin/dwm-opacity.sh 70
-~/.local/bin/wallpaper-and-colors.sh ~/Pictures/Wallpapers/fall-autumn-red-season.jpg
+sed -i "s/^.*\[SchemeNorm\].*/$(sed -n 3p "$HOME"/.cache/wal/colors-wal-dmenu.h)/" "$HOME"/Programs/dmenu/config.def.h
+sed -i "s/^.*\[SchemeSel\].*/$(sed -n 4p "$HOME"/.cache/wal/colors-wal-dmenu.h)/" "$HOME"/Programs/dmenu/config.def.h
+sed -i "s/^.*\[SchemeOut\].*/$(sed -n 5p "$HOME"/.cache/wal/colors-wal-dmenu.h)/" "$HOME"/Programs/dmenu/config.def.h
+colorNewHighlight=$(sed -n 7p "$HOME"/.cache/wal/colors)
+colorNewHighlight=$(echo "$colorNewHighlight" | sed "s/^/\"/")
+colorNewHighlight=$(echo "$colorNewHighlight" | sed "s/$/\"/")
+color2=$(grep "\[SchemeSel\] =" "$HOME"/Programs/dmenu/config.def.h)
+color2=$(echo "$color2" | sed "s/^.*, //")
+color2=${color2% \},}
+color3=$(grep "\[SchemeNorm\] =" "$HOME"/Programs/dmenu/config.def.h)
+color3=$(echo "$color3" | sed "s/^.*, //")
+color3=${color3% \},}
+sed -i "s/^.*\[SchemeSelHighlight\] =.*/        \[SchemeSelHighlight\] = \{ ${colorNewHighlight}, ${color2} \},/" "$HOME"/Programs/dmenu/config.def.h
+sed -i "s/^.*\[SchemeNormHighlight\] =.*/        \[SchemeNormHighlight\] = \{ ${colorNewHighlight}, ${color3} \},/" "$HOME"/Programs/dmenu/config.def.h
+
+cd "$HOME"/Programs/dwm/ && sudo make clean install
+cd "$HOME"/Programs/dmenu/ && sudo make clean install
 
 # Delete password given by archinstall.sh
 sed -i "s/^password=.*/password=password/" /home/"$username"/archsetup.sh
