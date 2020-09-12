03c890dd60d1dc4a5c8522767735b639ed6190e0
Paul Ritterbush <ritterbush@berkeley.edu>
2020-09-11 20:28:11 -0700

    Update archsetup.sh

--- archsetup.sh
+++ archsetup.sh
@@ -6,7 +6,7 @@ password=password
 username=username
 
 # Update pkg list
-(echo "$password"; echo "$password") | sudo -S pacman -Syu
+echo "$password" | sudo -S pacman -Syu
 
 # Xorg server, shell, terminal, editor, browser, varous packages my scripts use, and extras
 (echo; echo; echo) | sudo pacman -S xorg xorg-xinit zsh git alacritty neovim firefox picom xwallpaper sxiv python-pywal neofetch htop
@@ -17,33 +17,30 @@ curl https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg > ~/Pictu
 
 # Generate py-wal cache files before building dwm and dmenu
 wal -i ~/Pictures/Wallpapers/fall-autumn-red-season.jpg
+sleep 10
 
 # Directory for building programs from source
 mkdir ~/Programs
 
 # Get my dwm/dmenu desktop environment, various dotfiles, and scripts
 git clone https://github.com/ritterbush/files ~/Programs/
-sleep 10
-echo "$(pwd)"
-echo "$(ls ~/Programs)"
-echo "$(ls ~/Programs/files)"
 sleep 5
-# Move dwm and dmenu so colors will be set with py-wal before building
-mv ~/Programs/files/dwm ~/Programs/
-mv ~/Programs/files/dmenu ~/Programs/
+echo "$(pwd)"
+echo "$(ls -la ~/Programs)"
+sleep 10
 
 # xinitrc
-cp -f ~/Programs/files/.xinitrc ~/
+cp ~/Programs/.xinitrc ~/.xinitrc
 
 # zshrc
-cp -f ~/Programs/files/.zshrc ~/
+cp ~/Programs/.zshrc ~/.zshrc
 
 # change shell to zsh
 (echo "$password"; echo /bin/zsh) | chsh 
 
 # shell scripts, neovim config and plugins, alacritty config 
-cp -rf ~/Programs/files/.local ~/
-cp -rf ~/Programs/files/.config ~/
+cp -r ~/Programs/.local ~/.local
+cp -r ~/Programs/.config ~/.config
 
 # picom compositor config
 mkdir -p ~/.config/picom
@@ -58,7 +55,6 @@ cp /etc/xdg/picom.conf.example ~/.config/picom/picom.conf
 
 # Delete password given by archinstall.sh
 sed -i "s/password=.*/password=password/" /home/"$username"/archsetup.sh
-
 
 systemctl reboot
 
