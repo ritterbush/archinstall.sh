5db4ae540df9c123e6b9f06f60314a5cdcf08544
Paul Ritterbush <ritterbush@berkeley.edu>
2021-12-27 11:21:16 -0800

    Added help option. Removed Getops. Minor formatting.

--- archinstall.sh
+++ archinstall.sh
@@ -6,18 +6,38 @@
 # Without -w, it is assumed that you have already partitioned and mounted your drives correctly according to a uefi system
 # Options -a or -i will install AMD or Intel cpu microcode (pick one or neither to install no microcode)
 
-wipe=false # If -w option is used, wipes disk clean and makes partitions according to the below variables
-disk=sda # Wipes the disk '/dev/disk', can be changed with -d; use the letters that follow /dev/ and that specify the disk to wipe
-efipart="$disk"1 # Matches disk above but 1
-rootpart="$disk"2 # Match disk above but 2
-homepart="$disk"3 # Match disk above but 3
-full=false # If -f option used, fully install ComfyOS desktop, otherwise do a basic installation 
-timezone=America/Los_Angeles # Change with -t. To see options: ls /usr/share/zoneinfo
-cpu=other # Must be other or amd or intel
-hostname=arch # Change with -h
-staticip=127.0.1.1 # Change with -s
-username=paul # Change with -u
+show_usage(){
+    printf "Usage:\n\n  $0 [options [parameters]]\n"
+    printf "\n"
+    printf "Installs Arch Linux on UEFI systems.\n"
+    printf "\n"
+    printf "Options [parameters]:\n"
+    printf "\n"
+    printf "  -u|--username [username]   Specify username; if special characters are\n                             used use single quotes.\n"
+    printf "  -p|--password [password]   Specify password; if special characters are\n                             used use single quotes.\n"
+    printf "  -o|--hostname [hostname]   Specify hostname:~ the name of the computer\n                             of this operating system.\n"
+    printf "  -t|--timezone [timezone]   Specify timezone; use single quotes.\n"    printf "  -s|--staticip [staticip]   Specify local static ip address (setup with\n                             your router). Do not use if no local static\n                             ip address has been set up. Use single quotes.\n"
+    printf "  -f|--full                  Install ComfyOS setup after basic Arch\n                             installation.\n"
+    printf "  -a|--amdcpu                Use amd cpu microcode.\n"  
+    printf "  -i|--intelcpu              Use intel cpu microcode.\n"
+    printf "  --wipe-disk                Wipes the disk (ALL DATA ERASED!) of\n                             /dev/--diskname specified by -d|--diskname\n"
+    printf "  -d|--diskname [diskname]   Specify diskname to be wiped; e.g.\n                             'sdc'. Do not include /dev/. \n"  
+    printf "  -h|--help                  Print this help.\n"
+exit
+}
+
+username=newuser # Change with -u
 password=password # Change with -p
+timezone=America/Los_Angeles # Change with -t. To see options: ls /usr/share/zoneinfo
+hostname=arch # Change with -o
+staticip=127.0.1.1 # Change with -s
+full=false # If -f option used, fully install ComfyOS desktop, otherwise do a basic installation 
+cpu=other # Must be other or amd or intel
+wipe=false # If -w option is used, wipes disk clean and makes partitions according to the below variables
+disk=none # Wipes the disk '/dev/disk', can be changed with -d; use the letters that follow /dev/ and that specify the disk to wipe
+efipart="$disk"1 # Same name as disk above but with 1 at the end
+rootpart="$disk"2 # Same name as disk above but 2 instead of 1 at the end
+homepart="$disk"3 # Same name as disk above but 3 instead of 2 at the end
 
 <<COMMENT
         This script installs Arch Linux while making several assumptions: one, it installs only for UEFI systems. 
@@ -62,41 +82,109 @@ password=password # Change with -p
 
 COMMENT
 
-# I've added rudimentary getopts support
-# So now you can use any or all of the following options: 
-# -u username -p password -h -hostname -d disk -t timezone -s staticip
-# Also use -a, or -i for AMD or Intel cpu microcode
-
-while getopts ":u:p:h:d:t:s:aiwf" opt; do
-  case ${opt} in
-    u ) username=${OPTARG}
-      ;;
-    p ) password=${OPTARG}
-      ;;
-    h ) hostname=${OPTARG}
-      ;;
-    d ) disk=${OPTARG}
-      ;;
-    t ) timezone=${OPTARG}
-      ;;
-    s ) staticip=${OPTARG}
-      ;;
-    a ) cpu=amd
-      ;;
-    i ) cpu=intel
-      ;;
-    w ) wipe=true
-      ;;
-    f ) full=true
-      ;;
-  esac
+while [ -n "$1" ]; do
+    case "$1" in
+        --username|-u)
+            if [ -n "$2"  ]
+            then
+                username="$2"
+                shift 2
+            else
+                echo "-u flag requires a username"
+                exit
+            fi
+            ;;
+        --password|-p)
+            if [ -n "$2"  ]
+            then
+                password="$2"
+                shift 2
+            else
+                echo "-p option requires a password"
+                exit
+            fi
+            ;;
+        --hostname|-h)
+            if [ -n "$2"  ]
+            then
+                hostname="$2"
+                shift 2
+            else
+                echo "-h option requires a hostname"
+                exit
+            fi
+            ;;
+        --timezone|-t)
+            if [ -n "$2"  ]
+            then
+                timezone="$2"
+                shift 2
+            else
+                echo "-t option requires a timezone"
+                exit
+            fi
+            ;;
+        --staticip|-s)
+            if [ -n "$2"  ]
+            then
+                staticip="$2"
+                shift 2
+            else
+                echo "-s option requires a static ip address"
+                exit
+            fi
+            ;;
+        --full|-f)
+            full=true
+            shift
+            ;;
+        --amdcpu|-a)
+            cpu=amd
+            shift
+            ;;
+        --intelcpu|-i)
+            cpu=intel
+            shift
+            ;;
+        --wipe-disk)
+            wipe=true
+            shift
+            ;;
+        --diskname|-d)
+            if [ -n "$2"  ]
+            then
+                disk="$2"
+                shift 2
+            else
+                echo "-d option requires a diskname"
+                exit
+            fi
+            ;;
+        --help|-h)
+            show_usage
+            ;;
+        *)
+            echo "Unknown option $1"
+            show_usage
+            ;;
+    esac
 done
 
+[ $wipe = true ] && [ $disk = none ] && echo "Specify diskname with -d|--diskname when using --wipe-disk option"; exit
+
+echo "Past wipefs + disk check"
+exit 0
+exit 1
+exit 0
+exit;exit;exit
 # Update System Clock
 timedatectl set-ntp true
 
 if [ $wipe = true ] # -w option
 then
+
+    exit 0	
+
     # Wipe the disk, and in particular wipe the partitions previously made first, if this script has been already run 
     ls /dev/"$homepart" > /dev/null 2>&1 && wipefs --all --force /dev/"$homepart"
     sleep 1
